2885aa990f7109cdf019e26bc6e37e6a6934055d
iamsanaasayyed <iamsanaasayyed@gmail.com>
2023-12-27 00:42:16 +0530

    ui changes
    1. cafe screens
    a. free user
    b. paid user
    c. confirmation

--- src/app/v1-3/v1-3-routing.module.ts
+++ src/app/v1-3/v1-3-routing.module.ts
@@ -66,6 +66,10 @@ const routes: Routes = [
     path: 'authentication',
     loadChildren: () => import('./authentication/authentication.module').then( m => m.AuthenticationModule)
   },
+  {
+    path: 'cafe',
+    loadChildren: () => import('./cafe/cafe.module').then( m => m.CafeModule)
+  },
 ];
 
 @NgModule({
